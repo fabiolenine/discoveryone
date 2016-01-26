a7c5c4129bfd432104fad3d7d38afeedd85369e3
Fabio Lenine <fabiolenine@gmail.com>
2016-01-26 19:16:00 -0300

    Fix it
    Retificação

--- volatile.js
+++ volatile.js
@@ -1,7 +1,7 @@
 var app     = require('express')();
 var http    = require('http').Server(app);
 var io      = require('socket.io')(http);
-var socket  = require('.public/javascripts/volatilechat/socket.js')
+var socket  = require('./public/javascripts/volatilechat/socket.js')
 
 app.use(express.static('../views/volatilechat'));
 
