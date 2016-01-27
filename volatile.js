4870789359a78c3fd470a4285f55ecea43abb190
Fabio Lenine <fabiolenine@gmail.com>
2016-01-26 21:06:29 -0300

    Retificação
    Engine puro

--- volatile.js
+++ volatile.js
@@ -7,7 +7,7 @@ var socket  = require('./public/javascripts/volatilechat/socket.js');
 
 app.use(express.static('public'));
 app.set('views','views/volatilechat');
-app.engine('html', engines.swig);
+app.engine('html', engines);
 app.set('view engine', 'html');
 
 app.get('/', function(req, res){
