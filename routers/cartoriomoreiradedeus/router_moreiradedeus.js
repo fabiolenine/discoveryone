03702b20e0faa1f544fba8a39d22f0b301e49d5a
Fabio Lenine <fabiolenine@gmail.com>
2016-04-18 12:55:44 -0300

    Fix It
    Header CORS

--- routers/cartoriomoreiradedeus/router_moreiradedeus.js
+++ routers/cartoriomoreiradedeus/router_moreiradedeus.js
@@ -4,7 +4,7 @@ module.exports = function(app)
 {
 	
 	app.use(function(req,res,next){
-		res.header('Access-Control-Allow-Origin','*');
+		res.header('Access-Control-Allow-Origin','http://moreiradedeus.com.br');
 		next();
 	});
 	
