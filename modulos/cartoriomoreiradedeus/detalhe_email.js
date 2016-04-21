70c6d74bb6f6ea0857fc826bf444854bdb8d0fce
Fabio Lenine <fabiolenine@gmail.com>
2016-04-21 17:58:06 -0300

    Fix it
    Teste

--- modulos/cartoriomoreiradedeus/detalhe_email.js
+++ modulos/cartoriomoreiradedeus/detalhe_email.js
@@ -14,7 +14,7 @@ module.exports = function(nodemailer, smtpTransport){
 			//html: 	mensagem + '<br><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3981.341076875133!2d'+lat+'!3d'+loc+'!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zM8KwNDQnMDguMyJTIDM4wrAyOCcyMy4yIlc!5e0!3m2!1spt-BR!2sbr!4v1461077812486" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>'
 		};
 
-		var transporter = nodemailer.createTransport(smtpTransport,accountmail);
+		var transporter = nodemailer.createTransport(smtpTransport(accountmail));
 		
 		console.log(transporter);
 		
