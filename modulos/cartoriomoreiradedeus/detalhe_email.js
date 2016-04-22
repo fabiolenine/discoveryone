98b87a86ac56e763cd6096431b87177fdf3343af
Fabio Lenine <fabiolenine@gmail.com>
2016-04-22 18:48:49 -0300

    Debug
    teste

--- modulos/cartoriomoreiradedeus/detalhe_email.js
+++ modulos/cartoriomoreiradedeus/detalhe_email.js
@@ -14,10 +14,10 @@ module.exports = function(sendgrid, sendgridEMAIL){
 		//	//html: 	mensagem + '<br><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3981.341076875133!2d'+lat+'!3d'+loc+'!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zM8KwNDQnMDguMyJTIDM4wrAyOCcyMy4yIlc!5e0!3m2!1spt-BR!2sbr!4v1461077812486" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>'
 		//};
 
-		sendgridEMAIL.addTo('fabiolenine@icloud.com');
-		sendgridEMAIL.setFrom('fabiolenine@gmail.com');
-		sendgridEMAIL.setSubject('Teste');
-		sendgridEMAIL.setHtml('Olá eu sou o Bot');
+		//sendgridEMAIL.addTo('fabiolenine@icloud.com');
+		//sendgridEMAIL.setFrom('fabiolenine@gmail.com');
+		//sendgridEMAIL.setSubject('Teste');
+		//sendgridEMAIL.setHtml('Olá eu sou o Bot');
 		
 		//var transporter = nodemailer.createTransport(smtpTransport(accountmail));
 		
@@ -33,8 +33,16 @@ module.exports = function(sendgrid, sendgridEMAIL){
 		//	return callback(true);
 		//});
 		
-		sendgrid.send(sendgridEMAIL);
-		return true;
+		sendgrid.send({to: 'volatilechat@protonmail.ch',
+					  from:'fabiolenine@gmail.com',
+					  subject: 'Teste',
+					  text: 'Hi !'}, function(err, json){
+			if (err) {
+				console.error(err);
+				return false;}
+			console.log(json);
+			return true;
+		});
 	};
 	
 	var retorno = {"enviaremail" : enviaremail};
