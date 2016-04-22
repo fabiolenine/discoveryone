9e75f28e1c96c554b43e0fe8d14e128b57503ee4
Fabio Lenine <fabiolenine@gmail.com>
2016-04-22 19:46:05 -0300

    debug
    teste

--- modulos/cartoriomoreiradedeus/detalhe_email.js
+++ modulos/cartoriomoreiradedeus/detalhe_email.js
@@ -3,15 +3,13 @@ module.exports = function(sendgrid, sendgridEMAIL){
 	var enviaremail = function(nome, email, assunto, mensagem, 
 						    	//lat, loc
 							   callback){
-	
-		//html: 	mensagem + '<br><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3981.341076875133!2d'+lat+'!3d'+loc+'!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zM8KwNDQnMDguMyJTIDM4wrAyOCcyMy4yIlc!5e0!3m2!1spt-BR!2sbr!4v1461077812486" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>'
 
 		var params = {to: 		'volatilechat@protonmail.ch',
 					  from:		email,
 					  fromname:	nome,
 					  subject:	assunto,
 					  text: 	'Mensagem: ' + mensagem,
-					  html:		'<br><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3981.341076875133!2d'+lat+'!3d'+loc+'!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zM8KwNDQnMDguMyJTIDM4wrAyOCcyMy4yIlc!5e0!3m2!1spt-BR!2sbr!4v1461077812486" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>'
+					  html:		'<br><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3981.341076875133!2d-0'+lat+'!3d-0'+loc+'!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zM8KwNDQnMDguMyJTIDM4wrAyOCcyMy4yIlc!5e0!3m2!1spt-BR!2sbr!4v1461077812486" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>',
 					  replyto:	email}; 
 		
 		sendgrid.send(params, function(err, json){
