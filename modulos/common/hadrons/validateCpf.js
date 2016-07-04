db7320282e89299e6fe87481dc3047d133dbdf72
Fabio Lenine <fabiolenine@gmail.com>
2016-07-04 10:27:29 -0300

    Validação
    Retificando a variável

--- modulos/common/hadrons/validateCpf.js
+++ modulos/common/hadrons/validateCpf.js
@@ -4,18 +4,18 @@ module.exports = function(value){
     var Resto;
     Soma = 0;
     
-	for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+	for (i=1; i<=9; i++) Soma = Soma + parseInt(value.substring(i-1, i)) * (11 - i);
 	Resto = (Soma * 10) % 11;
 	
     if ((Resto == 10) || (Resto == 11))  Resto = 0;
-    if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
+    if (Resto != parseInt(value.substring(9, 10)) ) return false;
 	
 	Soma = 0;
-    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(value.substring(i-1, i)) * (12 - i);
     Resto = (Soma * 10) % 11;
 	
     if ((Resto == 10) || (Resto == 11))  Resto = 0;
-    if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
+    if (Resto != parseInt(value.substring(10, 11) ) ) return false;
 
 	return true;
 	
