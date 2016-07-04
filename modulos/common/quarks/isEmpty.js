8e2b5b432ac39b7d5cc0f2ec55dae89977ecd666
Fabio Lenine <fabiolenine@gmail.com>
2016-07-04 10:24:43 -0300

    Retificação
    Do novo para o antigo.

--- modulos/common/quarks/isEmpty.js
+++ modulos/common/quarks/isEmpty.js
@@ -1,6 +1,4 @@
-'use strict';
-
-module.exports = (value) => {
+module.exports = function(value){
   const isNull = (value === null);
   const isUndefined = (value === undefined);
   const isEmpty = (value === '');
