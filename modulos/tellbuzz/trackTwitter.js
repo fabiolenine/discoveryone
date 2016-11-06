8941595250104541f77faaf5fdbb9168dc3c28aa
Fabio Lenine <fabiolenine@gmail.com>
2016-11-06 19:09:20 -0300

    Change
    Acrescido mais tracks

--- modulos/tellbuzz/trackTwitter.js
+++ modulos/tellbuzz/trackTwitter.js
@@ -3,6 +3,8 @@ module.exports = function(tw, socket)
 		
 	tw.track('@veja');
 	tw.track('@exame');
+	tw.track('@ibmbrasil');
+	tw.track('@srlm');
 	tw.on('tweet', function(tweet){
 		socket.emit('buzz', tweet);
 	});
