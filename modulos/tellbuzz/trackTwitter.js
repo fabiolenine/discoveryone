ba2dacd68e2c235a60c35e5e67ffc8127ac75c4b
Fabio Lenine <fabiolenine@gmail.com>
2016-11-06 18:48:06 -0300

    Debug
    Retirada do console.log

--- modulos/tellbuzz/trackTwitter.js
+++ modulos/tellbuzz/trackTwitter.js
@@ -4,7 +4,6 @@ module.exports = function(tw, socket)
 	tw.track('Lula');
 	tw.track('Michel Temer');
 	tw.on('tweet', function(tweet){
-		console.log(tweet);
 		socket.emit('buzz', tweet);
 	});
 	
--- public/javascripts/tellbuzz/tellbuzzCTRLMain.js
+++ public/javascripts/tellbuzz/tellbuzzCTRLMain.js
@@ -1,7 +1,7 @@
 angular.module("tellbuzzCTRLMain",[])
 .controller('tellbuzzControllerMain', function($scope,$http,$window,$interval,$timeout) {
 		
-	var socket = io.connect('http://162.222.177.65');
+	var socket = io.connect('http://162.222.177.65:8080');
 	
 	socket.on('buzz', function (dados) {
 		console.log(dados);
