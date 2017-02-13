8e0ba07cf686fcfbd8ea65ea386ccbddf6437e02
Fabio Lenine <fabiolenine@gmail.com>
2017-02-13 00:22:21 -0300

    debug
    teste…

--- routers/cartoriomoreiradedeus/routerMoreiradedeus.js
+++ routers/cartoriomoreiradedeus/routerMoreiradedeus.js
@@ -1,7 +1,7 @@
 // Rota dos sites Cartório Moreira de Deus
 module.exports = function(app,detalheemails,dbcontatosite,dbpesquisar,dbdadospesquisa)
 {
-	app.set('views','views/moreiradedeus');
+	app.set('views','../../views/moreiradedeus');
 	
 	var isCPF 	= require('../../modulos/common/quarks/isCpf.js');
 	var isCNPJ	= require('../../modulos/common/quarks/isCnpj.js');
